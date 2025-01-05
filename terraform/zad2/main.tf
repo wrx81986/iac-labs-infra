140792d220d0860a4a8df38e398d7f94258da492
wrx81986 <wrx81986@student.wroclaw.merito.pl>
2025-01-05 15:03:15 +0100

    Update main.tf

--- terraform/zad2/main.tf
+++ terraform/zad2/main.tf
@@ -15,6 +15,13 @@ resource "docker_image" "nginx" {
   name         = "nginx:latest"
   keep_locally = false
 }
+
+resource "docker_tag" "tag_<indeks>" {
+  source_image = "existing_image_name"   # Nazwa obrazu źródłowego
+  target_image = "new_image_name"       # Nazwa obrazu docelowego
+  tag          = "new_tag"              # Nowy tag (np. v1.0)
+}
+
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
   name  = "tutorial"
